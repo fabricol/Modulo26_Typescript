16129167d0085987ed2548dd58ca78af1c69435e
fabricol <fabricioctnmiranda@gmail.com>
2024-05-10 12:34:12 -0300

    Resolução atividade

---
+++ pratique_modulo26.ts
@@ -0,0 +1,10 @@
+// Função 1
+const multiplicacao = (numero1:number, numero2:number): number => numero1 * numero2
+const resultado = multiplicacao(1, 2);
+console.log(resultado)
+//Função 2
+function OlaPessoa (pessoa: string): string {
+    return "Olá " + pessoa;
+    }
+const fabricio = OlaPessoa('fabricio')
+console.log(fabricio)
